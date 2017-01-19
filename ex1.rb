40b0003581f195690f87bb8c6a99a72383b8e8fb
Noora <n.k.alqasimi@gmail.com>
2017-01-19 17:52:46 +0000

    Added Exercise 1

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+# This is the first exercise
+puts "My name is Noora."
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
